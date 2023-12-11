2264e22ebbf2525b974136bfb6f472b3fec78836
MarkDVerhagen <markdverhagen@gmail.com>
2023-12-11 10:32:32 +0100

    Update 03_desc_tables

--- src/03_desc_tables.R
+++ src/03_desc_tables.R
@@ -1,25 +1,49 @@
-## Script to make descriptive tables
+####################################################################################
+# Script: Descriptive Tables Generation from Demographic Data
+# Description: This script generates create descriptive tables for the demographic
+#              data. It involves adding interacted variable groups, making year-wise
+#              counts, and grouping data based on various demographic characteristics.
+#
+# Input Files:
+#   1. Functions: './src/functions.R'
+#   2. Full Demographic Data (v3): 'data/edit/dem_full_v3.rda'
+#
+# Output Files:
+#   1. Numeric Descriptive Table: 'data/output/dem_table_num_v3.xlsx'
+#   2. Percentage Descriptive Table: 'data/output/dem_table_per_v3.xlsx'
+#
+# Libraries Used:
+#   - tidyverse
+#   - data.table
+#   - xlsx
+#
+# Author: Mark Verhagen
+# Date: 11-12-2023
+####################################################################################
+
 
 ## Load libraries
 library(tidyverse)
 library(data.table)
+source('./src/functions.R')
 
-## Load analysis data
-load("data/edit/full_analysis_sets.rda")
-gc()
+## Load demographic data
+load("data/edit/dem_full_v3.rda")
 
-rm(procedures)
-gc()
+## Make fully interacted demographic groups
+dem_full <- dem_full %>% add_interacted_var_group()
 
-tab <- dem_full %>% 
-  group_by(year) %>% 
-  count() %>% 
-  mutate(var_group = "Total",
-         var_name = "Total")
-  
+## Make year count
+tab <- dem_full[. .(n = .N), by = year]
+tab[, var_group := "Total"]
+tab[, var_name = "Total"]
 
-for (i in c("female", "age_group", "background_group",
-            "income_group", "poverty")) {
+
+## Grouping variables
+groups <- c("female", "age_group", "background_group", "income_group",
+            "poverty", "gih_interact", "ih_interact", "full_interact")
+
+for (i in groups) {
   x <- dem_full %>% 
     group_by(year, get(i)) %>% 
     count() %>% 
@@ -30,14 +54,16 @@ for (i in c("female", "age_group", "background_group",
   tab <- rbind(tab, x)
 }
 
-# numeric table  
+## Numeric table  
 tab <- tab %>% 
   pivot_wider(names_from = year, values_from = n)
 
-# save numeric table
-xlsx::write.xlsx(tab, "data/output/dem_table_num.xlsx")
+tab <- tab[!is.na(tab$`2017`), ]
 
-# percent_table 
+## Save numeric table
+xlsx::write.xlsx(tab, "data/output/dem_table_num_v3.xlsx")
+
+## Percent_table 
 total <- tab %>% filter(var_group == "Total")
 
 for (i in names(tab)) {
@@ -47,6 +73,6 @@ for (i in names(tab)) {
   }
 }
 
-# save percent table
-xlsx::write.xlsx(tab, "data/output/dem_table_per.xlsx")
+## Save percent table
+xlsx::write.xlsx(tab, "data/output/dem_table_per_v3.xlsx")
 
